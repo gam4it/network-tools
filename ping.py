4b19efffca28c580df8f76178fecaa657da5dafc
Gamma <gammalee@gmail.com>
2017-07-04 12:42:53 +1000

    apply Google Code Style and add TODOs

--- ping.py
+++ ping.py
@@ -3,6 +3,9 @@ import os
 import subprocess
 import ipaddress
 
+# TODO(gam4it) : Make ping.py as a module of a package called network-tools.
+# TODO(gam4it) : Make a separate module (or package) for ANSI color.
+
 # Prompt the user to input a network address
 net_addr = input("Enter a network address in CIDR format(ex.192.168.1.0/24): ")
 
@@ -13,20 +16,20 @@ ip_net = ipaddress.ip_network(net_addr)
 all_hosts = list(ip_net.hosts())
 
 # Configure subprocess to hide the console window
-startupinfo = None
+startup_info = None
 if os.name == 'nt':
-    startupinfo = subprocess.STARTUPINFO()
-    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-    startupinfo.wShowWindow = subprocess.SW_HIDE
+    startup_info = subprocess.STARTUPINFO()
+    startup_info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    startup_info.wShowWindow = subprocess.SW_HIDE
 
 # For each IP address in the subnet, 
 # run the ping command with subprocess.popen interface
 print("\n\033[1;31;40m Y \033[0m: Response, \033[0;30;47m N \033[0m: No response\n")
 for i in range(len(all_hosts)):
     if os.name == 'nt':
-        output = subprocess.Popen(['ping', '-n', '1', '-w', '500', str(all_hosts[i])], stdout=subprocess.PIPE, startupinfo=startupinfo).communicate()[0]
+        output = subprocess.Popen(['ping', '-n', '1', '-w', '500', str(all_hosts[i])], stdout=subprocess.PIPE, startupinfo=startup_info).communicate()[0]
     else:
-        output = subprocess.Popen(['ping', '-c', '3', '-t', '3', str(all_hosts[i])], stdout=subprocess.PIPE, startupinfo=startupinfo).communicate()[0]
+        output = subprocess.Popen(['ping', '-c', '3', '-t', '3', str(all_hosts[i])], stdout=subprocess.PIPE, startupinfo=startup_info).communicate()[0]
     
     if "Destination host unreachable" in output.decode('utf-8'):
         print(str(all_hosts[i]), "is N")
